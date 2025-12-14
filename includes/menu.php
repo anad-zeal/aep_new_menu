22948e6419fca7a6a86ec545717ab04e3015213f
anad-zeal <elza.dana@gmail.com>
2025-12-13 18:43:07 -0700

    Remove unnecessary closing brace for h6 styling in CSS

--- includes/menu.php
+++ includes/menu.php
@@ -1,16 +1,17 @@
 <nav id="main-nav" class="main-nav-menu" aria-label="Main Navigation">
 
-    <!-- Close Button (X) -->
-    <button id="close-nav-btn" class="close-nav-btn" aria-label="Close Menu">&times;</button>
+        <!-- Close Button (X) -->
+        <button id="close-nav-btn" class="close-nav-btn" aria-label="Close Menu">&times;</button>
 
-    <!-- <div class="hero" style="margin-bottom: 2rem; text-align: left;">
+        <!-- <div class="hero" style="margin-bottom: 2rem; text-align: left;">
             <div class="sub-title" style="font-size: 2rem;">Alexis Elza</div>
         </div> -->
 
-    <!-- Menu Links -->
-    <a href="/" data-page="home">Home</a>
-    <a href="/" data-page="artworks">Home</a>
-    <a href="/biography" data-page="biography">Biography</a>
-    <a href="/contact" data-page="contact">Contact</a>
+        <!-- Menu Links -->
+        <a href="/" data-page="home">Home</a>
+        <a href="/" data-page="artworks">Home</a>
+        <a href="/biography" data-page="biography">Biography</a>
+        <a href="/contact" data-page="contact">Contact</a>
+
 
 </nav>
