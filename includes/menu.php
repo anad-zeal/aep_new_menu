41310e5685ab934cd1aa3f11bf96df6cb439679d
anad-zeal <elza.dana@gmail.com>
2025-12-15 15:37:27 -0700

    Fix HTML structure in menu links for proper nesting

--- includes/menu.php
+++ includes/menu.php
@@ -9,9 +9,9 @@
 
         <!-- Menu Links -->
         <a href="/" data-page="home">Home</a>
-        <a href="/" data-page="artworks">Home</a>
-        <a href="/biography" data-page="biography">Biography</a>
-        <a href="/contact" data-page="contact">Contact</a>
+        <a href="/" data-page="artworks">Artworks/a>
+            <a href="/biography" data-page="biography">Biography</a>
+            <a href="/contact" data-page="contact">Contact</a>
     </div>
 
     <!-- Dark Backdrop -->
