dfd0af3bf293161e7170156e2a0f4c0869398615
anad-zeal <elza.dana@gmail.com>
2025-12-29 10:02:36 -0700

    Fix: add missing Comfortaa font link for improved typography

--- includes/header.php
+++ includes/header.php
@@ -56,6 +56,8 @@ $life_title = "<h2 class='sub-title fade-title'>The Life sof an Artist</h2>";
         href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Comfortaa:wght@300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
 
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap" rel="stylesheet">
+
     <!-- Stylesheets -->
     <link rel="stylesheet" href="/assets/css/style.css" />
 </head>
