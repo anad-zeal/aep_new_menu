5ca1bb31e64c649ac469e7194518f670451ce070
anad-zeal <elza.dana@gmail.com>
2026-01-09 11:24:21 -0700

    Refactor code structure for improved readability and maintainability

--- assets/js/modules/Gallery.js
+++ assets/js/modules/Gallery.js
@@ -45,7 +45,6 @@ export default class Gallery {
 
                     <div class="gallery-info">
                         <h3 id="image-title"></h3>
-                        <p id="image-caption"></p>
                     </div>
                 </div>
             </div>
@@ -54,7 +53,7 @@ export default class Gallery {
         // Cache DOM elements
         this.mainImage = document.getElementById('main-image');
         this.imageTitle = document.getElementById('image-title');
-        this.imageCaption = document.getElementById('image-caption');
+       // this.imageCaption = document.getElementById('image-caption');
         this.stageLoader = document.getElementById('stage-loader');
 
         // Event Listeners for Nav
