df08ecee97d644d80ec76b21a8d8ed4ce4b300d2
anad-zeal <elza.dana@gmail.com>
2026-01-15 15:39:20 -0700

    fix: correct HTML markup for category header in gallery module

--- assets/js/modules/Gallery.js
+++ assets/js/modules/Gallery.js
@@ -48,7 +48,7 @@ export default class Gallery {
             <div class="gallery-module">
                 <!-- Header -->
                 <div class="category">
-                    <p.logo>The Life of an Artist</p.logo>
+                    <p class="logo>The Life of an Artist</p>
                     <p>${this.categoryTitle}</p>
                 </div>
 
